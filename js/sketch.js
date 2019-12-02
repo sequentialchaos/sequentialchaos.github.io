5d4028ff9aa07a36aab39f70e25fb9608296f49b
Winston Smith <wsbraves@gmail.com>
2019-12-01 19:05:02 -0500

    remove hex grid background

---
+++ js/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+	createCanvas(innerWidth, innerHeight)
+	colorMode(HSB, 1, 1, 1, 1)
+	backgroundColor = color(0.5, 0.2, 0.9)
+}
+
+function draw() {
+	background(backgroundColor)
+}
